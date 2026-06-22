d23f8bdfb5497233c246d11088cf4ebca44ae6e7
Deepika Sharma <sharmadeepi200@gmail.com>
2026-06-23 02:23:16 +0530

    Update 0242-valid-anagram.cpp

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -56,6 +56,28 @@
 // };
 
 //run in single pass
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size() != t.size())
+//             return false;
+
+//         int cnt[26] = {0};
+
+//         for(int i = 0; i < s.size(); i++) {
+//             cnt[s[i] - 'a']++;
+//             cnt[t[i] - 'a']--;
+//         }
+
+//         for(int x : cnt)
+//             if(x != 0)
+//                 return false;
+
+//         return true;
+//     }
+// };
+
+//run in single pass
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -76,3 +98,4 @@ public:
         return true;
     }
 };
+
