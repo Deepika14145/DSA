8e95fef50edfb4f4198c88809017fd6a5983fb62
Deepika Sharma <sharmadeepi200@gmail.com>
2026-06-23 02:13:04 +0530

    Time: 1 ms (54.65%), Space: 9.8 MB (49.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //maintain hashtable
+        if(s.size() != t.size())
+            return false;
+
+        int cnt[26] = {0};
+
+        for(int i = 0; i< s.size(); i++)
+            cnt[s[i] - 'a']++;
+
+        for(int i = 0; i < t.size(); i++)
+            cnt[t[i] - 'a']--;
+
+        for(int i = 0; i<26; i++)       //check if any dissimilar char exists or not
+            if(cnt[i] != 0)
+                return false;
+
+        return true;
+        
+    }
+};
