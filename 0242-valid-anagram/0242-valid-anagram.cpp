40b0d51ddbd949123be85fc9e1eaf7b96cdfdb66
Deepika Sharma <sharmadeepi200@gmail.com>
2026-06-23 02:15:48 +0530

    Time: 8 ms (7.1%), Space: 9.8 MB (49.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,33 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         //maintain hashtable
+//         if(s.size() != t.size())
+//             return false;
+
+//         int cnt[26] = {0};
+
+//         for(int i = 0; i< s.size(); i++)
+//             cnt[s[i] - 'a']++;
+
+//         for(int i = 0; i < t.size(); i++)
+//             cnt[t[i] - 'a']--;
+
+//         for(int i = 0; i<26; i++)       //check if any dissimilar char exists or not
+//             if(cnt[i] != 0)
+//                 return false;
+
+//         return true;
+        
+//     }
+// };
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //maintain hashtable
-        if(s.size() != t.size())
-            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
-        int cnt[26] = {0};
-
-        for(int i = 0; i< s.size(); i++)
-            cnt[s[i] - 'a']++;
-
-        for(int i = 0; i < t.size(); i++)
-            cnt[t[i] - 'a']--;
-
-        for(int i = 0; i<26; i++)       //check if any dissimilar char exists or not
-            if(cnt[i] != 0)
-                return false;
-
-        return true;
-        
+        return s == t;
     }
 };
