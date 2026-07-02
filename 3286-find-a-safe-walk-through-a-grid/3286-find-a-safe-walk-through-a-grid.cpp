baf0e692304535581611ef2112598ffe1223da3a
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-02 23:05:29 +0530

    Time: 16 ms (67.95%), Space: 30.4 MB (91.68%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
+
+        dist[0][0] = grid[0][0];
+        pq.push({dist[0][0], {0, 0}});
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!pq.empty()) {
+            auto [cost, p] = pq.top();
+            pq.pop();
+
+            int r = p.first, c = p.second;
+
+            if (cost != dist[r][c]) continue;
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                    continue;
+
+                int newCost = cost + grid[nr][nc];
+
+                if (newCost < dist[nr][nc]) {
+                    dist[nr][nc] = newCost;
+                    pq.push({newCost, {nr, nc}});
+                }
+            }
+        }
+
+        return dist[m - 1][n - 1] < health;
+    }
+};
