f3bfdab47bbdfed9d35d80f795e704fb229ac1a8
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-09 14:02:01 +0530

    Time: 0 ms (100%), Space: 12.1 MB (97.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,56 +1,75 @@
+// class Solution {
+// public:
+//     void mergeArray(vector<int>& arr, int low, int mid, int high) {
+//         vector<int> temp;
+
+//         int i = low;
+//         int j = mid + 1;
+
+//         while (i <= mid && j <= high) {
+//             if (arr[i] <= arr[j])
+//                 temp.push_back(arr[i++]);
+//             else
+//                 temp.push_back(arr[j++]);
+//         }
+
+//         while (i <= mid)
+//             temp.push_back(arr[i++]);
+
+//         while (j <= high)
+//             temp.push_back(arr[j++]);
+
+//         for (int k = low; k <= high; k++)
+//             arr[k] = temp[k - low];
+//     }
+
+//     void mergeSort(vector<int>& arr, int low, int high) {
+//         if (low >= high)
+//             return;
+
+//         int mid = low + (high - low) / 2;
+
+//         mergeSort(arr, low, mid);
+//         mergeSort(arr, mid + 1, high);
+
+//         mergeArray(arr, low, mid, high);
+//     }
+
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         vector<int> temp;
+
+//         // Copy first m elements of nums1
+//         for (int i = 0; i < m; i++)
+//             temp.push_back(nums1[i]);
+
+//         // Copy all elements of nums2
+//         for (int i = 0; i < n; i++)
+//             temp.push_back(nums2[i]);
+
+//         // Sort using Merge Sort
+//         mergeSort(temp, 0, temp.size() - 1);
+
+//         // Copy back to nums1
+//         for (int i = 0; i < m + n; i++)
+//             nums1[i] = temp[i];
+//     }
+// };
+
 class Solution {
 public:
-    void mergeArray(vector<int>& arr, int low, int mid, int high) {
-        vector<int> temp;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        int i = low;
-        int j = mid + 1;
-
-        while (i <= mid && j <= high) {
-            if (arr[i] <= arr[j])
-                temp.push_back(arr[i++]);
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
             else
-                temp.push_back(arr[j++]);
+                nums1[k--] = nums2[j--];
         }
 
-        while (i <= mid)
-            temp.push_back(arr[i++]);
-
-        while (j <= high)
-            temp.push_back(arr[j++]);
-
-        for (int k = low; k <= high; k++)
-            arr[k] = temp[k - low];
-    }
-
-    void mergeSort(vector<int>& arr, int low, int high) {
-        if (low >= high)
-            return;
-
-        int mid = low + (high - low) / 2;
-
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-
-        mergeArray(arr, low, mid, high);
-    }
-
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> temp;
-
-        // Copy first m elements of nums1
-        for (int i = 0; i < m; i++)
-            temp.push_back(nums1[i]);
-
-        // Copy all elements of nums2
-        for (int i = 0; i < n; i++)
-            temp.push_back(nums2[i]);
-
-        // Sort using Merge Sort
-        mergeSort(temp, 0, temp.size() - 1);
-
-        // Copy back to nums1
-        for (int i = 0; i < m + n; i++)
-            nums1[i] = temp[i];
+        while (j >= 0)
+            nums1[k--] = nums2[j--];
     }
 };
