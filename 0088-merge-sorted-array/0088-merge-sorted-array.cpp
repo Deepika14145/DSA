992d6bb3fcaca4dfcdf17c08495d349ce5b2df51
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-09 13:59:22 +0530

    Time: 0 ms (100%), Space: 15.6 MB (9.5%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void mergeArray(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+
+        int i = low;
+        int j = mid + 1;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j])
+                temp.push_back(arr[i++]);
+            else
+                temp.push_back(arr[j++]);
+        }
+
+        while (i <= mid)
+            temp.push_back(arr[i++]);
+
+        while (j <= high)
+            temp.push_back(arr[j++]);
+
+        for (int k = low; k <= high; k++)
+            arr[k] = temp[k - low];
+    }
+
+    void mergeSort(vector<int>& arr, int low, int high) {
+        if (low >= high)
+            return;
+
+        int mid = low + (high - low) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        mergeArray(arr, low, mid, high);
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp;
+
+        // Copy first m elements of nums1
+        for (int i = 0; i < m; i++)
+            temp.push_back(nums1[i]);
+
+        // Copy all elements of nums2
+        for (int i = 0; i < n; i++)
+            temp.push_back(nums2[i]);
+
+        // Sort using Merge Sort
+        mergeSort(temp, 0, temp.size() - 1);
+
+        // Copy back to nums1
+        for (int i = 0; i < m + n; i++)
+            nums1[i] = temp[i];
+    }
+};
