9e264591bbbbcad649cce5fd2f8a8ba48fa9ed97
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-19 11:51:21 +0530

    Time: 4 ms (31.29%), Space: 10.8 MB (17.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0 || (x % 10 == 0 && x != 0)){
-            return false;
+        // if(x < 0 || (x % 10 == 0 && x != 0)){
+        //     return false;
+        // }
+
+        // int rev = 0;
+        //     while ( x > rev){
+        //         rev = rev * 10 + x % 10;
+        //         x /= 10;
+        //     }
+
+        //     return x == rev || x == rev / 10;
+
+        if(x < 0) return false;
+
+        string s = to_string(x);
+
+        int i = 0, j = s.size() - 1;
+
+        while(i < j){
+            if(s[i] != s[j])
+                return false;
+            i++;
+            j--;
         }
-
-        int rev = 0;
-            while ( x > rev){
-                rev = rev * 10 + x % 10;
-                x /= 10;
-            }
-
-            return x == rev || x == rev / 10;
+        return true;
     }
 };
