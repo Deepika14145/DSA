f5f066747afa313fd5d39ef7a8b8b732cf2c6a10
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-22 15:43:25 +0530

    Time: 37 ms (78.22%), Space: 45.5 MB (40.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,39 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int total_sub = 0;
+//         for(int i = 0; i<n; i++){
+//             int sum = 0;
+
+//             for(int j = i; j<n; j++){
+//                 sum += nums[j];
+
+//                 if(sum == k)
+//                     total_sub++;
+//             }
+//         }
+//         return total_sub;
+//     }
+// };
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int total_sub = 0;
-        for(int i = 0; i<n; i++){
-            int sum = 0;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
 
-            for(int j = i; j<n; j++){
-                sum += nums[j];
+        int prefixSum = 0, ans = 0;
 
-                if(sum == k)
-                    total_sub++;
-            }
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (mp.count(prefixSum - k))
+                ans += mp[prefixSum - k];
+
+            mp[prefixSum]++;
         }
-        return total_sub;
+
+        return ans;
     }
 };
