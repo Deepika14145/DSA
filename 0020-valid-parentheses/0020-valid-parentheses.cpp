cb2cac788c0d0d5df50aa41a1ad50650c7adca4b
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-28 22:41:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (6.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,42 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+
+//         for (char ch : s) {
+//             if (ch == '(' || ch == '{' || ch == '[') {
+//                 st.push(ch);
+//             } else {
+//                 if (st.empty()) return false;
+
+//                 if ((ch == ')' && st.top() != '(') ||
+//                     (ch == '}' && st.top() != '{') ||
+//                     (ch == ']' && st.top() != '['))
+//                     return false;
+
+//                 st.pop();
+//             }
+//         }
+
+//         return st.empty();
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
 
         for (char ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else {
-                if (st.empty()) return false;
-
-                if ((ch == ')' && st.top() != '(') ||
-                    (ch == '}' && st.top() != '{') ||
-                    (ch == ']' && st.top() != '['))
+            if (ch == '(')
+                st.push(')');
+            else if (ch == '{')
+                st.push('}');
+            else if (ch == '[')
+                st.push(']');
+            else {
+                if (st.empty() || st.top() != ch)
                     return false;
-
                 st.pop();
             }
         }
