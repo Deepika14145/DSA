bfe5f396934707a0a7ae55a044bb866ad6a593d2
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-30 16:28:24 +0530

    Time: 60 ms (16.69%), Space: 153.1 MB (16.56%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
+        priority_queue<pair<int, int>> pq;
         vector<int> ans;
 
         for (int i = 0; i < nums.size(); i++) {
+            // Push current element and its index
+            pq.push({nums[i], i});
 
-            // Remove indices outside current window
-            while (!dq.empty() && dq.front() <= i - k)
-                dq.pop_front();
+            // Remove elements outside the current window
+            while (!pq.empty() && pq.top().second <= i - k) {
+                pq.pop();
+            }
 
-            // Remove smaller elements
-            while (!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
-
-            dq.push_back(i);
-
-            // First window completed
-            if (i >= k - 1)
-                ans.push_back(nums[dq.front()]);
+            // Window is complete
+            if (i >= k - 1) {
+                ans.push_back(pq.top().first);
+            }
         }
 
         return ans;
