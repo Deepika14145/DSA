d4483d57a1c211d360368d8b163567e1031da488
Deepika Sharma <sharmadeepi200@gmail.com>
2026-07-30 22:32:37 +0530

    Time: 5 ms (59.51%), Space: 13.5 MB (83.87%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-       stack <char> st; 
+    //    stack <char> st; 
 
-       for(char ch : s){
-        if(!st.empty() && st.top() == ch)
-            st.pop();
-        else
-            st.push(ch);
-       }
+    //    for(char ch : s){
+    //     if(!st.empty() && st.top() == ch)
+    //         st.pop();
+    //     else
+    //         st.push(ch);
+    //    }
 
-       string ans = "";
+    //    string ans = "";
 
-       while(!st.empty()){
-        ans += st.top();
-        st.pop();
-       }
+    //    while(!st.empty()){
+    //     ans += st.top();
+    //     st.pop();
+    //    }
 
-       reverse(ans.begin(), ans.end());
+    //    reverse(ans.begin(), ans.end());
 
-       return ans;
+       //return ans;
+
+       string ans;
+
+for (char ch : s) {
+    if (!ans.empty() && ans.back() == ch)
+        ans.pop_back();
+    else
+        ans.push_back(ch);
+}
+
+return ans;
     }
 };
