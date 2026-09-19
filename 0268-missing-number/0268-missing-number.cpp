1dd821fa519e1f8afa3301ad9ee610a4db2ec06c
Deepika Sharma <sharmadeepi200@gmail.com>
2026-09-19 16:44:47 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,28 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+
+//         for(int i = 0; i<n; i++){
+//             if(nums[i] != i)
+//                 return i;
+//         }
+//         return n;
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
 
-        for(int i = 0; i<n; i++){
-            if(nums[i] != i)
-                return i;
-        }
-        return n;
+        int expected = n * (n + 1) / 2;
+
+        int actual = 0;
+        for (int x : nums)
+            actual += x;
+
+        return expected - actual;
     }
 };
